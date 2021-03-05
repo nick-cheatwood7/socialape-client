8b71809d020d732f18645627f269e83f06a36076
nick-cheatwood7 <nick.cheatwood@gmail.com>
2021-03-04 18:58:12 -0600

    Daily commit

---
+++ src/pages/home.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react'
+import axios from 'axios'
+import Grid from '@material-ui/core/Grid'
+
+import Scream from '../components/Scream'
+
+class home extends Component {
+
+  state = {
+    screams: null
+  };
+
+  componentDidMount() {
+    // get screams from server
+    axios
+      .get('/screams')
+      .then((res) => {
+        this.setState({
+          screams: res.data
+        })
+      })
+      .catch(err => console.error(err))
+  }
+
+  render() {
+
+    const {screams} = this.state
+
+    console.log(screams)
+
+    let recentScreamsMarkup = screams ? (
+      screams.map((scream) => <Scream key={scream.screamId} scream={scream}/>)
+    ) : (
+        <p>Loading...</p>
+    )
+
+    return (
+      <Grid container spacing={16}>
+        <Grid item sm={8} xs={12}>
+          {recentScreamsMarkup}
+        </Grid>
+        <Grid item sm={4} xs={12}>
+          <p>Profile...</p>
+        </Grid>
+      </Grid>
+    )
+  }
+}
+
+export default home
